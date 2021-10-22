74864a9397633db2ced5eb2fe4219c4e0e14f71c
KliasUnknown <92763761+KliasUnknown@users.noreply.github.com>
2021-10-21 21:32:37 -0700

    Added radius claim limit

--- src/main/java/com/massivecraft/factions/cmd/claim/CmdClaim.java
+++ src/main/java/com/massivecraft/factions/cmd/claim/CmdClaim.java
@@ -61,6 +61,11 @@ public class CmdClaim extends FCommand {
         int i = 0;
         if (radius >= 2) i = 1;
 
+        if(radius > Conf.claimRadiusLimit && Conf.claimRadiusLimit != -1 && !context.fPlayer.isAdminBypassing()) {
+            context.msg(TL.COMMAND_CLAIM_RADIUSOVER, Conf.claimRadiusLimit);
+            return;
+        }
+
         Faction at = Board.getInstance().getFactionAt(new FLocation(context.fPlayer.getPlayer().getLocation()));
 
         switch (i) {
